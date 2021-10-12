e5f7f3440aa0e4d5af7d42d7830b8709b99cef06
Taku Takamatsu <59686013+taku-takamatsu@users.noreply.github.com>
2021-10-12 11:31:40 -0400

    Delete yelp-api/.history directory

--- yelp-api/.history/main_20210929181051.py
+++
@@ -1,7 +0,0 @@
-from YelpApi import YelpApi
-import os
-
-if __name__ == __main__:
-    print(os.environ['YELP_API_CLIENT_ID'])
-    print(os.environ['YELP_API_KEY'])
-    # api = YelpApi()
--- yelp-api/.history/main_20210929181207.py
+++
@@ -1,9 +0,0 @@
-import YelpApi
-import os
-
-def get_data():
-    print(os.environ['YELP_API_CLIENT_ID'])
-    print(os.environ['YELP_API_KEY'])
-
-get_data()
-# api = YelpApi()
